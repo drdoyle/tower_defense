8f45776a1416ef33756e6fb6b75102cc0a8539e3
drdoyle <doylejackd@gmail.com>
2019-08-29 19:07:40 -0400

    Moved the list of animation frames from a self.imgs to a class variables imgs to reduce memory load when many instances of a class are on the screen.

--- enemies/enemy_1.py
+++ enemies/enemy_1.py
@@ -4,13 +4,15 @@ from .enemy import Enemy
 
 class En_1(Enemy):
 
+    imgs = [
+        pygame.transform.scale(
+            pygame.image.load(
+                os.path.join("game_assets", "enemies", "1", "1_enemies_1_run_" + str(x) + ".png")),
+            (64, 64)) for x in range(4)
+    ]
+
     def __init__(self, speed):
         super().__init__(speed=speed)
         self.max_health = 1
         self.health = self.max_health
-        self.imgs = [
-            pygame.transform.scale(
-                pygame.image.load(
-                    os.path.join("game_assets", "enemies", "1", "1_enemies_1_run_" + str(x) + ".png")),
-                (64, 64)) for x in range(4)
-        ]
+
--- enemies/enemy_2.py
+++ enemies/enemy_2.py
@@ -4,15 +4,14 @@ from .enemy import Enemy
 
 class En_2(Enemy):
 
-
-
-    def __init__(self, speed):
-        super().__init__(speed=speed)
-        self.max_health = 3
-        self.health = self.max_health
-        self.imgs = [
+    imgs = [
             pygame.transform.scale(
                 pygame.image.load(
                     os.path.join("game_assets", "enemies", "2", "2_enemies_1_run_" + str(x) + ".png")),
                 (64, 64)) for x in range(4)
         ]
+
+    def __init__(self, speed):
+        super().__init__(speed=speed)
+        self.max_health = 3
+        self.health = self.max_health
