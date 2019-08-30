7e15e16cc0d49238c7edb387e136075a17b65ef7
drdoyle <42181067+drdoyle@users.noreply.github.com>
2019-08-30 17:03:09 -0400

    Moved buttons to their own file and imported them to keep behavior cleaner. Changed position to be center-based like other images Changed button drawing to use a subsurface built from the menu itself Added "adding names to name list" to the add_button function Renamed get_clicked() to pass_click() to better capture the spirit of the function; checking where the click has happened and passing that information to the appropriate button to do what it's supposed to Fixed an off-by-one error with the add_button() functions of the Horizontal and Vertical subclasses to properly calculate the increments by which to place the buttons. Added the archerTowerMenu subclass with image information and constructor which builds the three buttons those menus use

--- menu.py
+++ menu.py
@@ -1,54 +1,21 @@
 import pygame
-
-
-class Button:
-    def __init__(self, x, y, img, name):
-        self.name = name
-        self.img = img
-        self.center_x = x
-        self.center_y = y
-        self.width = self.img.get_width()
-        self.height = self.height.get_width()
-
-        self.x = self.center_x - self.width/2
-        self.y = self.center_y - self.height/2
-
-    def click(self, pos):
-        return self.x <= pos[0] <= self.x + self.width and self.y <= pos[1] <= self.y + self.height
-
-    def do_click(self):
-        """
-        Placeholder function that will be overwritten by individual buttons' functionality.
-        :return: None
-        """
-        return NotImplementedError
-
-    def draw(self, win):
-        win.blit(self.img, (self.x, self.y))
-
-    def set_pos(self, x, y):
-        """
-        Changes the x-position of the center of the button to a new value
-        :param x: Int
-        :return: None
-        """
-        self.center_x = x
-        self.center_y = y
-        self.x = self.center_x - self.width/2
-        self.y = self.center_y - self.height / 2
+import os
+from button import Button
 
 
 class Menu:
+    img = None
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.width = 0
-        self.height = 0
+        self.center_x = x
+        self.center_y = y
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        self.x = self.center_x - self.width/2
+        self.y = self.center_y + self.height/2
 
         self.item_names = []
         self.buttons = []
-        self.imgs = []
         self.items = len(self.item_names)
 
     def click(self, pos):
@@ -60,16 +27,31 @@ class Menu:
         return self.x <= pos[0] <= self.x + self.width and self.y <= pos[0] <= self.y + self.height
 
     def draw(self, win):
+        win.blit(self.img, (self.x, self.y))
+        subwin = win.subsurface(self.img.get_rect(topleft=(self.x, self.y)))
         for b in self.buttons:
-            b.draw(win)
+            b.draw(subwin)
 
     def add_button(self, img, name):
+        """
+        Incomplete method that should be extended by the subclass in order to arrange the buttons.
+        :param img: Surface
+        :param name: String
+        :return: None
+        """
         self.items += 1
         new_button = Button(x=0, y=0, img=img, name=name)
         self.buttons.append(new_button)
+        self.item_names.append(new_button.name)
         # something done in a subclass
 
-    def get_clicked(self, pos):
+    def pass_click(self, pos):
+        """
+        Passes the mouse click along to each of its buttons to see if any of them are triggered by it, and if so,
+        then do whatever needs to be done.
+        :param pos: (Int, Int)
+        :return: None
+        """
         for b in self.buttons:
             if b.click(b, pos):
                 b.do_click()
@@ -87,9 +69,9 @@ class HorizontalMenu(Menu):
         :return: None
         """
         super().add_button(img, name)
-        inc_x = self.width / self.items
+        inc_x = self.width / (self.items + 1)
         # update the x-position of each button in our list
-        for b in enumerate(self.buttons):
+        for b in enumerate(self.buttons, 1):
             b[1].set_pos(x=inc_x*b[0], y=self.height/2)
 
 
@@ -105,7 +87,29 @@ class VerticalMenu(Menu):
         :return: None
         """
         super().add_button(img, name)
-        inc_y = self.height / self.items
+        inc_y = self.height / (self.items + 1)
         # update the position of each button in our list
-        for b in enumerate(self.buttons):
+        for b in enumerate(self.buttons, 1):
             b[1].set_pos(x=self.width/2, y=inc_y * b[0])
+
+
+class archerTowerMenu(HorizontalMenu):
+    img = pygame.transform.scale(
+                pygame.image.load(os.path.join("game_assets", "menu_bg.png")),
+                (300, 64))
+    btn_plus = pygame.transform.scale(
+                    pygame.image.load(os.path.join("game_assets", "button_plus.png")),
+                    (64, 48))
+    btn_sell = pygame.transform.scale(
+                    pygame.image.load(os.path.join("game_assets", "button_money.png")),
+                    (64, 48))
+    btn_info = pygame.transform.scale(
+                    pygame.image.load(os.path.join("game_assets", "button_info.png")),
+                    (64, 48))
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        for name, btn in {"plus": self.btn_plus, "sell": self.btn_sell, "info": self.btn_info}.items():
+            self.add_button(img=btn, name=name)
+
+
