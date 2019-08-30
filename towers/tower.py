169e034e61cb261e190b05444b523a238b5d7483
drdoyle <42181067+drdoyle@users.noreply.github.com>
2019-08-30 08:55:02 -0400

    Created dummy class variables to prettify code in editor. Changed self.draw() to only show range if tower is selected. Added start of menu drawing. Cleaned up self.click() check and return. Sell() now flags tower for deletion and returns sell price. Implemented self.get_upgrade_cost() and self.get_sell_price() so they shouldn't need to be repeated in subclasses. Changed self.set_range() to apply a change to the range rather than setting it equal to a value so we can apply layers of boosts without needing to do the math elsewhere. Might need to rename or delete this function later?

--- towers/tower.py
+++ towers/tower.py
@@ -1,5 +1,5 @@
 import pygame
-from math import atan2, degrees
+
 
 class Tower:
     """
@@ -8,18 +8,33 @@ class Tower:
     width = 64
     height = 64
 
+    # <editor-fold desc="These class lists are included to make the code look prettier,
+    # each subclass should have its own class list">
+    tower_imgs = []
+
+    base_damage = 0
+    base_range = 0
+    damage_list = []
+    range_list = []
+
+    upgrade_costs = [0]  # always have 0 at the end to signal that it can't be upgraded any further
+    sell_price = []
+    buy_price = []
+    # </editor-fold>
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.offset = (self.width / 2, self.height / 2)
 
-        self.range = 100
-
-        self.sell_price = [0, 0, 0]
-        self.buy_price = [0, 0, 0]
         self.level = 0
         self.selected = False
         self.menu = None
+        self.to_delete = False
+
+        self.damage = self.base_damage
+        self.range = self.base_range
+        self.img = self.tower_imgs[self.level]
 
     def draw(self, win):
         """
@@ -27,59 +42,88 @@ class Tower:
         :param win: Surface
         :return: None
         """
-        win.blit(self.tower_imgs[self.level], (self.x - self.offset[0], self.y - self.offset[1]))
+        win.blit(self.img, (self.x - self.offset[0], self.y - self.offset[1]))
+        if self.selected is True:
+            # draw range indicator
+            pygame.draw.circle(win, (0, 255, 0), (self.x, self.y), self.range, 4)
+            # draw menus
+            for m in self.menu:
+                m.draw()
 
-    def click(self, x_c, y_c):
+    def click(self, pos):
         """
-        Returns True if tower has been clicked on, if tower was clicked on selects tower
-        :param x_c: float
-        :param y_c: float
+        Returns True if tower has been clicked on.
+        :param pos: (Int, Int)
         :return: bool
         """
-        if x_c <= self.x + self.width and x >= self.x:
-            if y_c <= self.y + self.height and y_c >= self.y:
-                return True
-        return False
+        return self.x + self.width >= pos[0] >= self.x and self.y + self.height >= pos[1] >= self.y
 
     def sell(self):
         """
-        Call to sell the tower, returns the sell price.
+        Call to sell the tower, returns the sell price. Flags the tower for deletion during the next cleanup step.
         :return:
         """
+        self.to_delete = True
         return self.sell_price[self.level]
 
     def upgrade(self):
         """
-        Upgrades the tower for a given cost
+        Upgrades the tower for a given cost; overwritten by subclasses
         :return:
         """
-        pass
+        return NotImplementedError  # should never return this, included to catch if we forgot to override
 
     def get_upgrade_cost(self):
         """
         Returns the upgrade cost. Returns -1 if it can't upgrade further
-        :return:
+        :return: Int
         """
+        return self.upgrade_costs[self.level]
 
     def get_sell_price(self):
         """
         Returns the sell cost. Returns -1 if at base level.
         :return: int
         """
-        try:
-            price = self.sell_price[self.level]
-        except IndexError:
-            return -1
-        else:
-            return price
+
+        return self.sell_price[self.level]
 
     def move(self):
         pass
 
-    def set_range(self, newrange):
+    def set_range(self, new_range):  # TODO: is this method necessary?
         """
-        Setter for the range of the tower.
-        :param newrange: int
+        Setter for the range of the tower, primarily used in self.upgrade()
+        :param new_range: Int
         :return: None
         """
-        self.range = newrange
+        if new_range <= 0:
+            raise ValueError("Variable new_range must be positive.")
+        self.range = new_range
+
+    def change_range(self, range_boost):
+        """
+        Changer for the range of the tower, to allow for things like support towers which passively
+        boost the range of towers around them. Negative range_boosts decrease the range.
+        :param range_boost: Int
+        :return: None
+        """
+        self.range = max(0, self.range + range_boost)  # caps the range from below so it never becomes negative
+
+    def change_damage(self, dmg_boost):
+        """
+        Changer for the damage of the tower, to allow for support towers. Negative boosts decrease damage.
+        :param dmg_boost: Int
+        :return: None
+        """
+        self.damage = max(0, self.damage + dmg_boost)
+
+    def upgrade(self):
+        """
+        Applies all of the changes to the tower associated with going up a level
+        :return:
+        """
+        self.level += 1
+        self.change_damage(self.damage_list[self.level])
+        self.change_range(self.range_list[self.level])
+        self.img = self.tower_imgs[self.level]
